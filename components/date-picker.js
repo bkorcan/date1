80a946f89e2d19b78a0257a20c4b565fe6eba470
bkorcan <bkorcan@gmail.com>
2022-02-28 13:59:21 +0300

    102

--- components/date-picker.js
+++ components/date-picker.js
@@ -1,15 +1,32 @@
 import Style from '../styles/date.module.css'
 import { DayPicker } from 'react-day-picker';
 import 'react-day-picker/dist/style.css';
+import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
+import { useStore } from '../components/state'
 
 
 
 
  function Datepicker(){
 
+    const checkInText = useStore(state => state.checkInText)
+    const setCheckInText = useStore(state => state.setCheckInText)
+
+    const show = useStore(state => state.show)
+    const setShow = useStore(state => state.setShow)
+
     return(
         <div className={Style.container} >
-            <DayPicker  className={Style.dayContainer}  />
+            <div className={Style.checkInContainer} >
+                <CalendarTodayIcon  color='primary' />
+                {checkInText}
+            </div>
+
+            <div className={Style.dayContainer} >
+                <DayPicker  />
+            </div>
+            
+            
         </div>
         
     )
--- components/state.js
+++ components/state.js
@@ -1,6 +1,14 @@
 import create from 'zustand'
 
 const useStore = create(set => ({
-  x: 0,
-  setX: (v) => set(state => ({ x:v })),
+    x: 0,
+    setX: (v) => set(state => ({ x: v })),
+
+    show: 'none',
+    setShow: (v) => set(state => ({ show: v })),
+
+    checkInText: 'Check In',
+    setCheckInText: (v) => set(state => ({ checkInText: v })),
+
 }))
+export { useStore }
